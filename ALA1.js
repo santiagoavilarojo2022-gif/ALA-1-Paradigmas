21fa6169bb68dad05b3f4fa8ba59f727429ac9b6
Santiago <santiagoavilarojo2022@gmail.com>
2026-09-06 10:14:39 -0300

    primer commit

---
+++ ALA1.js
@@ -0,0 +1,37 @@
+const prompt =require('prompt-sync')();
+let tarea ={
+ titulo:"",
+ descripcion:"",
+ estado:"",
+ fecha:"",
+ vencimiento:"",
+ dificultad:"",
+}
+ //buscar la operacion push
+console.log("Hola, que desea?");
+do {
+console.log("----MENU-----");
+let op = parseInt (prompt("[1] ver tareas \n"+ //parseint toma el string y lo tranforma en un numero
+    "[2] Buscar Tarea\n"+
+    "[3] Agregar tarea\n"+
+    "[0] Salir\n" 
+))
+
+switch (op){
+    case 1: 
+    break;
+
+    case 2:
+        break;
+
+    case 3:
+        break;
+    case 0:
+        break;
+}
+
+
+
+} while (op == 0){
+    console.log("Adios");
+}
